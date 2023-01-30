1990ce649c1ed67a7b8af3d43445a15883c2e65f
demilyngo <deminguru01@gmail.com>
2023-01-30 21:25:21 +0500

    30.01 + markup

--- src/main/java/com/rapberry/pi4led/controller/LedController.java
+++ src/main/java/com/rapberry/pi4led/controller/LedController.java
@@ -2,8 +2,12 @@ package com.rapberry.pi4led.controller;
 
 import com.pi4j.io.gpio.*;
 import com.rapberry.pi4led.threads.ListenThread;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 
 @RestController
 public class LedController {
@@ -11,13 +15,17 @@ public class LedController {
     Integer msg = 13;
 
     @RequestMapping("/")
-    public String greeting() throws InterruptedException {
+    public String greeting(Model model) throws InterruptedException {
         while(messageController.getControl() == MessageController.Control.FIELD) {
             Thread.sleep(1);
         }
         messageController.sendMessage(0);
         messageController.sendMessage(msg);
-        return Integer.toBinaryString(msg);
+        //return Integer.toBinaryString(msg);
+        ArrayList<MessageController> data = new ArrayList<MessageController>();
+        data.add(messageController);
+        model.addAttribute("data", data);
+        return "index";
     }
 
 }
