1b577cd5c1873087415706956d8c4a0a8686e7f9
demilyngo <79042685+demilyngo@users.noreply.github.com>
2023-02-02 14:59:25 +0500

    Add files via upload

--- src/main/java/com/rapberry/pi4led/controller/LedController.java
+++ src/main/java/com/rapberry/pi4led/controller/LedController.java
@@ -3,7 +3,7 @@ package com.rapberry.pi4led.controller;
 import com.pi4j.io.gpio.*;
 import com.rapberry.pi4led.threads.ListenThread;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ public class LedController {
     final MessageController messageController = new MessageController();
     Integer msg = 13;
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String greeting() throws InterruptedException {
 
         messageController.sendMessage(0);
