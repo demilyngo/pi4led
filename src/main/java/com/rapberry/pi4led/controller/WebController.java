90c599c1198854f9ae286e33aa7f18bad71872f6
demilyngo <79042685+demilyngo@users.noreply.github.com>
2023-03-24 10:01:29 +0500

    Add files via upload

--- src/main/java/com/rapberry/pi4led/controller/WebController.java
+++ src/main/java/com/rapberry/pi4led/controller/WebController.java
@@ -16,7 +16,7 @@ public class WebController {
     @RequestMapping("/")
     public String greeting(Model model) throws InterruptedException {
         final StationController stationController = new StationController(State.SORTING, Control.SERVER, 3, "Сургутская");
-        Integer msg = 13;
+        //Integer msg = 13;
         System.out.println("get");
         ArrayList<StationController> station = new ArrayList<StationController>();
         station.add(stationController);
@@ -31,10 +31,10 @@ public class WebController {
         }
         model.addAttribute("counter", counter);
 
-        System.out.println("before sending");
-        stationController.sendMessage(msg);
-        System.out.println("after sending");
-        stationController.sendMessage(0);
+//        System.out.println("before sending");
+//        stationController.sendMessage(msg);
+//        System.out.println("after sending");
+//        stationController.sendMessage(0);
 
         return "index";
     }
