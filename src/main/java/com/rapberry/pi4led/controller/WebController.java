4bae578387d94852575c6520e9b5e7d2fa947f80
demilyngo <deminguru01@gmail.com>
2023-04-23 19:21:37 +0500

    + maket

--- src/main/java/com/rapberry/pi4led/controller/WebController.java
+++ src/main/java/com/rapberry/pi4led/controller/WebController.java
@@ -3,19 +3,23 @@ package com.rapberry.pi4led.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Controller
 public class WebController {
 
+    final StationController stationController = new StationController(State.WAITING, Control.SERVER, 0, "Сургутская");
 
     @RequestMapping("/")
     public String greeting(Model model) throws InterruptedException {
-        final StationController stationController = new StationController(State.SORTING, Control.SERVER, 3, "Сургутская");
         //Integer msg = 13;
         ArrayList<StationController> station = new ArrayList<StationController>();
         station.add(stationController);
@@ -24,11 +28,11 @@ public class WebController {
         ArrayList<String> cities = new ArrayList<String>(Arrays.asList("Москва", "Казань", "Нижневартовск", "Воркута"));
         model.addAttribute("cities", cities);
 
-        ArrayList<wagonModel> counter = new ArrayList<wagonModel>();
-        for(int i=1; i!=15; i++) {
-            counter.add(new wagonModel(i, cities.get((int) (Math.random() * 4))));
+        ArrayList<wagonModel> wagonList = new ArrayList<wagonModel>();
+        for(int i=1; i!=stationController.getTrainCounter(); i++) {
+            wagonList.add(new wagonModel(i, cities.get((int) (Math.random() * 4))));
         }
-        model.addAttribute("counter", counter);
+        model.addAttribute("wagonList", wagonList);
 
 //        System.out.println("before sending");
 //        stationController.sendMessage(msg);
@@ -36,6 +40,21 @@ public class WebController {
 //        stationController.sendMessage(0);
 
         return "index";
+    }
+
+
+    @ResponseBody
+    @PostMapping("/")
+    public String startSorting(@RequestBody String data) {
+        Pattern pattern = Pattern.compile("[0-6]");
+        String[] text = pattern.split(data);
+        Matcher matcher = pattern.matcher(data);
+        String res = "";
+        while (matcher.find()) {
+            res = res + (data.substring(matcher.start(), matcher.end()));
+        }
+        return res;
+    }
     }
 
 }
